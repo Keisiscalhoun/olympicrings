a9bb8b5ef5f7dbe112867a50f8cf65f2d001d217
28calhounk <28calhounk@gmail.com>
2025-03-07 08:41:43 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,120 @@
+import turtle
+
+screen = turtle.Screen()
+screen.screensize=(500,500)
+screen.bgcolor('tan')
+
+t = turtle.Turtle()
+#move
+
+t.pencolor('blue')
+t.penup()
+t.goto(-80,15)
+t.pendown()
+
+
+t.circle(35)
+
+#move
+
+t.pencolor('black')
+t.penup()
+t.goto(0 ,15)
+t.pendown()
+
+t.circle(35)
+
+#move
+
+t.pencolor('red')
+t.penup()
+t.goto(80 ,15)
+t.pendown()
+
+t.circle(35)
+
+
+#move
+
+t.pencolor('yellow')
+t.penup()
+t.goto(-40 ,-20)
+t.pendown()
+
+t.circle(35)
+
+
+#move
+
+t.pencolor('green')
+t.penup()
+t.goto(40 ,-20)
+t.pendown()
+
+t.circle(35)
+
+
+#move
+
+t.pencolor('purple')
+t.penup()
+t.goto(-20 , 150)
+t.pendown()
+
+t.write("Winter olympics")
+screen = turtle.Screen()
+screen.screensize=(500,500)
+
+
+#move
+
+t.pencolor('purple')
+t.penup()
+t.goto(-20 , -150)
+t.pendown()
+
+t.write("2026")
+screen = turtle.Screen()
+screen.screensize=(500,500)
+
+
+
+
+
+
+
+turtle.done()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
